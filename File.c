6badcfb32f72fb80f92f690a5f6462525dd7713e
ilyass cherradi <ilyasscherradi15@gmail.com>
2025-12-05 13:09:44 +0100

    added hash

--- File.c
+++ File.c
@@ -4,3 +4,15 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
+
+void hashage() {
+    // Fonction de hachage simple (exemple)
+    const char *str = "example";
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    printf("Hash: %lu\n", hash);
+}
